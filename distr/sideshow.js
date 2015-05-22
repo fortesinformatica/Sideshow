6b508ab1af0a37847afd27ef1656131e19eb2d75
Alberto Monteiro <alberto.monteiro@live.com>
2015-05-21 23:31:35 -0300

    Fixing copyright

--- distr/sideshow.js
+++ distr/sideshow.js
@@ -3,7 +3,7 @@
  Sideshow - An incredible Javascript interactive help Library
  Version: 0.4.0
  Date: 2014-11-27
- Author: Alcides Queiroz [alcidesqueiroz(at)gmail(dot)com]
+ Author: Fortes Informática
  Available under Apache License 2.0 (https://raw2.github.com/fortesinformatica/sideshow/master/LICENSE)
  **/
 
